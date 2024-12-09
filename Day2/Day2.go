f7eeeb04523eb0b98a188a12e890267b57082e6f
ManonLittleMouse <manon.sourisseau@ens-rennes>
2024-12-09 22:45:41 +0100

    Day2

--- Day2/Day2.go
+++ Day2/Day2.go
@@ -2,49 +2,82 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
-type Matrix struct {
-	Rows int
-	Cols int
-	data []int
-}
-
-func (m *Matrix) At(row, col int) int {
-	return m.data[row*m.Cols+col]
-}
-
-func SafeRow(m *Matrix, r int) bool {
+func SafeRow(row []int) bool {
 	var is_safe = true
 	var l_aux []int
 	l_aux = append(l_aux, 0)
-	for i := 1; i < m.Cols; i++ {
-		l_aux = append(l_aux, m.At(r, i-1)-m.At(r, i))
+	for i := 1; i < len(row); i++ {
+		l_aux = append(l_aux, row[i-1]-row[i])
 	}
-	fmt.Println(l_aux)
+	////fmt.Println("\t", l_aux)
 	for i := 1; i < len(l_aux); i++ {
-		var diff = int(math.Abs(float64(l_aux[i] - l_aux[i-1])))
-		if diff == 0 || diff > 3 || (l_aux[i]*l_aux[i-1] < 0) {
+
+		var diff = l_aux[i]
+		///fmt.Println(diff, " ")
+		if diff == 0 || math.Abs(float64(diff)) > 3 || (l_aux[i]*l_aux[i-1] < 0) {
 			is_safe = false
+
+		}
+	}
+	return is_safe
+}
+
+func Dampener(row []int) bool {
+	var is_safe = false
+	for i := 0; i < len(row); i++ {
+		var l_aux []int
+		for j := 0; j < len(row); j++ {
+			if i != j {
+				l_aux = append(l_aux, row[j])
+			}
+		}
+		b := SafeRow(l_aux)
+		if b == true {
+			is_safe = true
 		}
 	}
 	return is_safe
 }
 
 func main() {
-	var input = &Matrix{
-		Rows: 6,
-		Cols: 5,
-		data: []int{7, 6, 4, 2, 1, 1, 2, 7, 8, 9, 9, 7, 6, 2, 1, 1, 3, 2, 4, 5, 8, 6, 4, 4, 1, 1, 3, 6, 7, 9},
+
+	var rows, cols int
+	file, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
-	var count = 0
-	for r := 0; r < input.Rows; r++ {
-		var safe = SafeRow(input, r)
+	lines := strings.Split(string(file), "\n")
+	rows = len(lines)
+
+	count := 0
+
+	for i := 0; i < rows-1; i++ {
+		line := strings.Split(lines[i], " ")
+		cols = len(line)
+		var input []int
+		for j := 0; j < cols; j++ {
+			el, _ := strconv.Atoi(line[j])
+			input = append(input, el)
+		}
+		/// Part one
+		///var safe = SafeRow(input)
+		/// Part two
+		var safe = Dampener(input)
+		fmt.Println(input, "---", safe, "\n")
+
 		if safe == true {
 			count += 1
 		}
+
 	}
+	fmt.Println(rows)
 	fmt.Println(count)
 
 }
